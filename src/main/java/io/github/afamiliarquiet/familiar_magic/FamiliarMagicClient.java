12e8a79e07e71215417aeefcc1e26da7f0f362d9
afamiliarquiet <afamiliarquiet@gmail.com>
2024-10-27 11:27:06 -0700

    neoforgely focus

--- src/main/java/io/github/afamiliarquiet/familiar_magic/FamiliarMagicClient.java
+++ src/main/java/io/github/afamiliarquiet/familiar_magic/FamiliarMagicClient.java
@@ -1,17 +1,32 @@
 package io.github.afamiliarquiet.familiar_magic;
 
 import com.mojang.blaze3d.platform.InputConstants;
+import io.github.afamiliarquiet.familiar_magic.client.*;
 import io.github.afamiliarquiet.familiar_magic.network.FocusPayload;
 import net.minecraft.client.KeyMapping;
+import net.minecraft.client.model.*;
+import net.minecraft.client.renderer.ItemInHandRenderer;
+import net.minecraft.client.renderer.entity.EntityRenderer;
+import net.minecraft.client.renderer.entity.LivingEntityRenderer;
 import net.minecraft.client.resources.model.ModelResourceLocation;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.animal.Cat;
+import net.minecraft.world.entity.animal.Fox;
+import net.minecraft.world.entity.animal.Parrot;
+import net.minecraft.world.entity.animal.Wolf;
+import net.minecraft.world.entity.animal.frog.Frog;
+import net.minecraft.world.entity.monster.Silverfish;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.neoforge.client.event.ClientTickEvent;
+import net.neoforged.neoforge.client.event.EntityRenderersEvent;
+import net.neoforged.neoforge.client.event.RegisterGuiLayersEvent;
 import net.neoforged.neoforge.client.event.RegisterKeyMappingsEvent;
+import net.neoforged.neoforge.client.gui.VanillaGuiLayers;
 import net.neoforged.neoforge.client.settings.KeyConflictContext;
 import net.neoforged.neoforge.common.util.Lazy;
 import net.neoforged.neoforge.event.entity.player.PlayerEvent;
@@ -26,6 +41,7 @@ import static io.github.afamiliarquiet.familiar_magic.FamiliarMagic.MOD_ID;
 public class FamiliarMagicClient {
     public static final ModelResourceLocation BIG_HAT_ON_HEAD_MODEL = ModelResourceLocation.inventory(ResourceLocation.fromNamespaceAndPath(MOD_ID, "big_hat_on_head"));
     public static final ResourceLocation FOCUS_OVERLAY = ResourceLocation.fromNamespaceAndPath(MOD_ID, "textures/misc/focus.png");
+    public static final ResourceLocation FOCUS_LAYER = ResourceLocation.fromNamespaceAndPath(MOD_ID, "focus");
 
     public static final Lazy<KeyMapping> FOCUS_MAPPING = Lazy.of(() ->
             new KeyMapping(
@@ -52,7 +68,8 @@ public class FamiliarMagicClient {
 
     public FamiliarMagicClient(IEventBus modClientEventBus) {
         modClientEventBus.addListener(FamiliarMagicClient::mrwRegisterKeyMappingsEvent);
-        //modClientEventBus.addListener(FamiliarMagicClient::mrwClientTickEventPost);
+        modClientEventBus.addListener(FamiliarMagicClient::mrwRegisterGuiLayersEvent);
+//        modClientEventBus.addListener(FamiliarMagicClient::mrwEntityRenderersEventAddLayers);
 
         FOCUS_HELD_LAST_TICK.set(false); // does this matter? idk, whatever
     }
@@ -62,6 +79,45 @@ public class FamiliarMagicClient {
         event.register(FOCUS_MAPPING.get());
         event.register(FOCUS_TOGGLE_MAPPING.get());
     }
+
+    private static void mrwRegisterGuiLayersEvent(RegisterGuiLayersEvent event) {
+        event.registerAbove(VanillaGuiLayers.CAMERA_OVERLAYS, FOCUS_LAYER, new FocusRenderLayer());
+    }
+
+    // i'd love to take the events route instead of mixin spam but it gets mad at the casting so idk how to add layers
+//    private static void mrwEntityRenderersEventAddLayers(EntityRenderersEvent.AddLayers event) {
+//        ItemInHandRenderer handyHattyRenderer = event.getContext().getItemInHandRenderer();
+//
+//        EntityRenderer<Cat> catEntityRenderer = event.getRenderer(EntityType.CAT);
+//        if (catEntityRenderer instanceof LivingEntityRenderer<Cat, CatModel<Cat>> catRenderer) {
+//            catRenderer.addLayer(new CatHatLayer(catRenderer, handyHattyRenderer));
+//        }
+//
+//        EntityRenderer<Fox> foxEntityRenderer = event.getRenderer(EntityType.FOX);
+//        if (foxEntityRenderer instanceof LivingEntityRenderer<Fox, FoxModel<Fox>> foxRenderer) {
+//            foxRenderer.addLayer(new FoxHatLayer(foxRenderer, handyHattyRenderer));
+//        }
+//
+//        EntityRenderer<Frog> frogEntityRenderer = event.getRenderer(EntityType.FROG);
+//        if (frogEntityRenderer instanceof LivingEntityRenderer<Frog, FrogModel<Frog>> frogRenderer) {
+//            frogRenderer.addLayer(new FrogHatLayer(frogRenderer, handyHattyRenderer));
+//        }
+//
+//        EntityRenderer<Parrot> parrotEntityRenderer = event.getRenderer(EntityType.PARROT);
+//        if (parrotEntityRenderer instanceof LivingEntityRenderer<Parrot, ParrotModel> parrotRenderer) {
+//            parrotRenderer.addLayer(new ParrotHatLayer(parrotRenderer, handyHattyRenderer));
+//        }
+//
+//        EntityRenderer<Silverfish> silverfishEntityRenderer = event.getRenderer(EntityType.SILVERFISH);
+//        if (silverfishEntityRenderer instanceof LivingEntityRenderer<Silverfish, SilverfishModel<Silverfish>> silverfishRenderer) {
+//            silverfishRenderer.addLayer(new SilverfishHatLayer(silverfishRenderer, handyHattyRenderer));
+//        }
+//
+//        EntityRenderer<Wolf> wolfEntityRenderer = event.getRenderer(EntityType.WOLF);
+//        if (wolfEntityRenderer instanceof LivingEntityRenderer<Wolf, WolfModel<Wolf>> wolfRenderer) {
+//            wolfRenderer.addLayer(new WolfHatLayer(wolfRenderer, handyHattyRenderer));
+//        }
+//    }
 
     @EventBusSubscriber(bus = EventBusSubscriber.Bus.GAME, modid = MOD_ID, value = Dist.CLIENT)
     public static class ArghImGoingToBlowUp {
---
+++ src/main/java/io/github/afamiliarquiet/familiar_magic/client/FocusRenderLayer.java
@@ -0,0 +1,50 @@
+package io.github.afamiliarquiet.familiar_magic.client;
+
+import com.mojang.blaze3d.systems.RenderSystem;
+import io.github.afamiliarquiet.familiar_magic.FamiliarMagicClient;
+import net.minecraft.client.DeltaTracker;
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.LayeredDraw;
+import net.minecraft.util.Mth;
+
+import javax.annotation.ParametersAreNonnullByDefault;
+
+@ParametersAreNonnullByDefault
+public class FocusRenderLayer implements LayeredDraw.Layer {
+    private float focusIntensity = 0.0f;
+    @Override
+    public void render(GuiGraphics guiGraphics, DeltaTracker deltaTracker) {
+        if (FamiliarMagicClient.FOCUSED_LAST_TICK.get()) {
+            focusIntensity = Mth.lerp(
+                    0.5F * deltaTracker.getGameTimeDeltaTicks(),
+                    this.focusIntensity,
+                    1f);
+            renderFocusOverlay(guiGraphics, focusIntensity);
+            if (focusIntensity >= 0.99f) {
+                focusIntensity = 1.0f;
+            }
+        } else if (focusIntensity > 0.0f) {
+            focusIntensity = Mth.lerp(
+                    0.5F * deltaTracker.getGameTimeDeltaTicks(),
+                    this.focusIntensity,
+                    0f);
+            renderFocusOverlay(guiGraphics, focusIntensity);
+            if (focusIntensity <= 0.01f) {
+                focusIntensity = 0.0f;
+            }
+        }
+    }
+
+    // eat my access tf
+    private void renderFocusOverlay(GuiGraphics guiGraphics, float alpha) {
+        RenderSystem.disableDepthTest();
+        RenderSystem.depthMask(false);
+        RenderSystem.enableBlend();
+        guiGraphics.setColor(1.0F, 1.0F, 1.0F, alpha);
+        guiGraphics.blit(FamiliarMagicClient.FOCUS_OVERLAY, 0, 0, -90, 0.0F, 0.0F, guiGraphics.guiWidth(), guiGraphics.guiHeight(), guiGraphics.guiWidth(), guiGraphics.guiHeight());
+        RenderSystem.disableBlend();
+        RenderSystem.depthMask(true);
+        RenderSystem.enableDepthTest();
+        guiGraphics.setColor(1.0F, 1.0F, 1.0F, 1.0F);
+    }
+}
